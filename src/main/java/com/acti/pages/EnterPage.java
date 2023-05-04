00646e190814dea2e04f043ce472ba8eb41e7661
ktesf <ktesf@Naomi>
2023-05-03 20:58:28 -0500

    added  enter page locators and methods

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -1,5 +1,9 @@
 package com.acti.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.acti.base.DriverScript;
 
 /*
@@ -11,5 +15,41 @@ import com.acti.base.DriverScript;
  */
 
 public class EnterPage extends DriverScript{
+	
+	//***************************page Locators***************************//
+	
+		@FindBy(id = "logoutLink") WebElement logoutLink;
+		@FindBy(xpath = "//a[@class='userProfileLink username ']") WebElement userLoggedText;
+		@FindBy(xpath = "//div[@id='container_tasks']") WebElement menuTask;
+		@FindBy(xpath = "//div[@id='container_reports']") WebElement menuReports;
+		@FindBy(xpath = "//div[@id='container_users']") WebElement menuUsers;
+	//***************************page Initialization********************//
+		
+		public EnterPage()
+		{
+			PageFactory.initElements(driver, this);
+		}
+		
+		
+	//***************************page Methods**************************//
+		
+		public void clickLogout()
+		{
+			logoutLink.click();
+		}
+		
+		public String getUserLoggedIn()
+		{
+			return  userLoggedText.getText();
+		}
+		
+		public void clickTaskMenu()
+		{
+			menuTask.click();	
+		}
+			
+		
+			
+		
 
 }
